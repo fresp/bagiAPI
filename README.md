86c0eec888d1fb8e320e18aa282b6754e898396e
Freza Nugraha <frezanugraha404@gmail.com>
2020-02-05 16:55:35 +0700

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# bagiAPI
- 
+# bagiData API Test Case
+
+## Project setup
+```
+npm install
+```
+
+## Migrate Schema Sequalize
+```
+npx sequelize-cli db:migrate
+```
+
+## URL
+```
+endpoint : localhost:6969
+socket   : localhost:6969/www
+```
--- configs/database.js
+++ configs/database.js
@@ -1,14 +1,2 @@
-module.exports = {
-  mongo: {
-    development: {
-      host: 'mongodb://eannov8.xyz:27017/oneplus',
-    },
-    staging: {
-      host: 'mongodb://localhost:27017/oneplus',
-    },
-    production: {
-      host: 'localhost',
-    }
-  }
-}
+module.exports = {}
   
