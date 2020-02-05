98f1c01e39a1618e154160965b8926175052e8cc
Freza Nugraha <frezanugraha404@gmail.com>
2020-02-05 16:50:18 +0700

    clean code node use (1)

--- www/routers/index.js
+++ www/routers/index.js
@@ -1,11 +1,4 @@
 import app from "express";
 const wwwRouter = app.Router();
 
-// Import Router
-const userRouter = require("./user");
-const videoRouter = require("./video");
-
-
-wwwRouter.use('/video', videoRouter);
-wwwRouter.use('/user', userRouter);
 module.exports = wwwRouter;
